28193df6021c3e7ad3bd243cebb487ec85d97522
Pawel Lenart <pawel.lenart@schange.com>
2022-01-06 17:41:02 +0100

    Update head and logos

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { getSession } from 'next-auth/react';
 import Center from '../components/Center';
 import Player from '../components/Player';
@@ -6,6 +7,10 @@ import Sidebar from '../components/Sidebar';
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
+      <Head>
+        <title>Spotify 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main className="flex">
         <Sidebar />
         <Center />
--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,21 @@
 import { getProviders, signIn } from 'next-auth/react';
+import Head from 'next/head';
+import Image from 'next/image';
+import spotify from '../public/spotify.png';
 
 function Login({ providers }) {
   return (
     <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
-      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
+      <Head>
+        <title>Spotify 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Image src={spotify} width={200} height={200} alt="Spotify logo" />
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
           <button
-            className="bg-[#18D860] text-white p-5 rounded-full"
+            className="bg-[#18D860] text-white p-5 rounded-full mt-5"
             onClick={() => signIn(provider.id, { callbackUrl: '/' })}
           >
             Login with {provider.name}
